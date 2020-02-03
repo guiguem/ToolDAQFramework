4de74b3ff6dbd0fedd1ecbf308d88ab43d4fce10
Mathieu Guigue <mguigue@lpnhe.in2p3.fr>
2020-02-03 12:34:57 +0900

    Cleaning main

--- src/exe/main.cpp
+++ src/exe/main.cpp
@@ -15,39 +15,7 @@ int main(int argc, char* argv[]){
     ToolChain tools(conffile);
 
     std::cout << "Done here" << std::endl;
-
-  //DummyTool dummytool;    
-
-  //tools.Add("DummyTool",&dummytool,"configfiles/DummyToolConfig");
-
-  //int portnum=24000;
-  //  tools.Remote(portnum);
-  //tools.Interactive();
-  
-  //  tools.Initialise();
-  // tools.Execute();
-  //tools.Finalise();
-  
   
   return 0;
   
 }
-
-
-// #include <iostream>
-// #include <string>
-
-// #include "ToolBox.h"
-
-// int main()
-// {
-//     std::cout << "main" << std::endl;
-//     ToolBox toolchain;
-//     auto wrench = toolchain.CreateTool("Wrench", "splash");
-//     // auto wrench2 = toolchain.CreateTool("Wrench", "bing");
-//     // auto screwDriver = toolchain.CreateTool("ScrewDriver", "paff");
-//     // wrench->makeNoise();
-//     // wrench2->makeNoise();
-//     // screwDriver->makeNoise();
-//     return 0;
-// }
